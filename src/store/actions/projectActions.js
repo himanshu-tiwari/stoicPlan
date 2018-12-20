25ed2151755180ba656f0b7a9b3f3e408d7e9adb
himanshu-tiwari <htiwarih@gmail.com>
2018-12-20 13:33:43 +0530

    Connected react to firestore

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,5 +1,5 @@
 export const createProject = (project) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
         // make async calls to the db
         dispatch({ type: 'CREATE_PROJECT', project });
     };
