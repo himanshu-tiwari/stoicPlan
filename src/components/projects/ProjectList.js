123c66ade80da51c2ac89303aaf7993403b5b76a
himanshu-tiwari <htiwarih@gmail.com>
2018-12-24 19:09:43 +0530

    Tiny fix

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ProjectSummary from './ProjectSummary';
+import { Link } from 'react-router-dom';
 
 const ProjectList = ({ projects }) => {
     const projectList = projects && projects.length > 0 ? (
-        projects.map(project => <ProjectSummary project={project} key={project.id} />)
+        projects.map(project => <Link to={'/project/' + project.id} key={project.id}><ProjectSummary project={project} /></Link>)
     ) : (
         <div className="card z-depth-0 project-summary" key="project1">
             <div className="card-content black-text">
