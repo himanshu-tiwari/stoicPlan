9a14d394b97e02ef8a2bbfec39521b524ed864f7
himanshu-tiwari <htiwarih@gmail.com>
2018-12-25 17:12:07 +0530

    Syncing the author data with the projects

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -5,7 +5,7 @@ const ProjectSummary = ({ project }) => {
         <div className="card z-depth-0 project-summary">
             <div className="card-content black-text">
                 <span className="card-title">{project.title}</span>
-                <p>{project.content}</p>
+                <p>Posted by { project.authorFirstName } { project.authorLastName }</p>
                 <p className="grey-text">December 19 at 12:30 pm</p>
             </div>
         </div>
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -2,11 +2,14 @@ export const createProject = (project) => {
     return (dispatch, getState, { getFirebase, getFirestore }) => {
         // make async calls to the db
         const firestore = getFirestore();
+        const profile = getState().firebase.profile;
+        const authorId = getState().firebase.auth.uid;
+
         firestore.collection('projects').add({
             ...project,
-            authorFirstName: 'Stoic',
-            authorLastName: 'Bruhmin',
-            authorId: 12345,
+            authorFirstName: profile.firstName,
+            authorLastName: profile.lastName,
+            authorId,
             createdAt: new Date()
         }).then(() => {
             dispatch({ type: 'CREATE_PROJECT', project });
